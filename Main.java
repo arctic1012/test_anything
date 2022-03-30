620d17a3727994067b280a78ed1c7eba5139f982
JKJK <arctic-sky@naver.com>
2022-03-30 16:56:32 +0900

    Modifid code in Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello");
-    }
-}
+        System.out.println("hello");
+        int a = 5;
+
+        Scanner scan = new Scanner(System.in);
+        String answer = null;
+
+        System.out.println("for test run..?  Y/N: ");
+
+        answer = scan.nextLine();
+
+        System.out.println(answer);
+
+        if (answer.equals("Y")) {
+            for (int i = 1; i < 10; i++) {
+                int sum = a + i;
+                System.out.println(sum + " <<for grammer test");
+            }
+        }else{
+                System.out.println("program exited.");
+
+                scan.close();
+
+            }
+
+    }}
