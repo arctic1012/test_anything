ddf60c2c83e1417349b7885941304ec5c346036b
JKJK <arctic-sky@naver.com>
2022-04-13 16:39:09 +0900

    test

--- Main.java
+++ Main.java
@@ -1,32 +1,11 @@
-package com.company;
+class test_anything{
 
-import java.util.Scanner;
+static int a;
+public static void main(String[]args){
 
-public class Main {
+test_anything tst = new test_anything;
+a=5;
+Syste.out.println("test"+a);
+}
 
-    public static void main(String[] args) {
-        System.out.println("hello");
-        int a = 3;
-
-        Scanner scan = new Scanner(System.in);
-        String answer = null;
-
-        System.out.println("for test run..?  Y/N: ");
-
-        ans = scan.nextLine();
-
-        System.out.println(ans);
-
-        if (ans.equals("Y")) {
-            for (int i = 1; i < 10; i++) {
-                int sum = a + i;
-                System.out.println(sum + " <<for grammer test");
-            }
-        }else{
-                System.out.println("program exited.");
-
-                scan.close();
-
-            }
-
-    }}
+}
